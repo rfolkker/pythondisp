fa6a3845e8b2f089f9589412ff7c2604dc2770d3
Darnell Otterson <darnell.otterson@rcis.com>
2017-12-06 08:01:13 -0600

    Python is picky

--- pythondisp.py
+++ pythondisp.py
@@ -19,45 +19,47 @@ app = Flask(__name__)
 api = Api(app)
 
 class set_text(Resource):
-  def get(self, index, text):
-    print("Index: "+index)
-    print("Text: "+text)
-    offset=int(index)-1
-    print("offset: {}".format(offset))
-    print("Testing: "+lines[0])
-    lines[offset]=text
-    print("Text: "+lines[offset])
-    try:
-      papirus_text.AddText(lines[offset],0,offset*14,Id=ids[offset], size=12)
-    except:
-      print("What the heck?")
-    #text.WriteAll()
-    #result = {'data':dict(zip(sz.value.StatusZeroValue))}
-    return jsonify({'data':lines})
+    def get(self, index, text):
+        print("Index: "+index)
+        print("Text: "+text)
+        offset = int(index)-1
+        print("offset: {}".format(offset))
+        print("Testing: "+lines[0])
+        lines[offset] = text
+        print("Text: "+lines[offset])
+        try:
+            papirus_text.AddText( \
+                lines[offset], 0, offset*14, Id=ids[offset], size=12)
+        except:
+            print("What the heck?")
+        #text.WriteAll()
+        #result = {'data':dict(zip(sz.value.StatusZeroValue))}
+        return jsonify({'data':lines})
 
 class clear_text(Resource):
-  def get(self, index):
-    print("Index: "+index)
-    offset=int(index)-1
-    print("offset: {}".format(offset))
-    lines[offset]="" 
-    papirus_text.AddText(lines[offset],0,offset*14,Id=ids[offset+1], size=12)
-    #text.WriteAll()
-    #result = {'data':dict(zip(sz.value.StatusZeroValue))}
-    return jsonify({'data':lines})
+    def get(self, index):
+        print("Index: "+index)
+        offset = int(index)-1
+        print("offset: {}".format(offset))
+        lines[offset] = ""
+        papirus_text.AddText( \
+            lines[offset], 0, offset*14, Id=ids[offset+1], size=12)
+        #text.WriteAll()
+        #result = {'data':dict(zip(sz.value.StatusZeroValue))}
+        return jsonify({'data':lines})
 
 class commit_text(Resource):
-  def get(self, index):
-    try:
-      papirus_text.WriteAll()
-    except:
-      print("What the heck?")
-    #result = {'data':dict(zip(sz.value.StatusZeroValue))}
-    return jsonify({'data':lines})
+    def get(self, index):
+        try:
+            papirus_text.WriteAll()
+        except:
+            print("What the heck?")
+        #result = {'data':dict(zip(sz.value.StatusZeroValue))}
+        return jsonify({'data':lines})
 
 class get_text(Resource):
-  def get(self):
-    return jsonify({'data':lines})
+    def get(self):
+        return jsonify({'data':lines})
 
 api.add_resource(get_text, "/status")
 api.add_resource(set_text, '/<index>/<text>') #Dynamic route
@@ -67,9 +69,9 @@ api.add_resource(commit_text, '/commit')
 # Routes
 @app.route('/')
 def index():
-  return render_template("index.html")
+    return render_template("index.html")
 
 if __name__ == '__main__':
-  papirus_text.AddText(lines[0], 0, 0, Id=ids[0], size=12)
-  papirus_text.WriteAll()
-  app.run(host='0.0.0.0', port=5555)
+    papirus_text.AddText(lines[0], 0, 0, Id=ids[0], size=12)
+    papirus_text.WriteAll()
+    app.run(host='0.0.0.0', port=5555)
